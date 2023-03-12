f1c6ac9a97c90e84af13dd69639440db19b96a3d
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-03-12 11:13:42 -0700

    Fixing the spacing

--- SwiftCalendar/Persistence.swift
+++ SwiftCalendar/Persistence.swift
@@ -8,17 +8,22 @@
 import CoreData
 
 struct PersistenceController {
+    
     static let shared = PersistenceController()
 
     static var preview: PersistenceController = {
+        
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
+        
+        for _ in 0 ..< 10 {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
         }
+        
         do {
             try viewContext.save()
+            
         } catch {
             // Replace this implementation with code to handle the error appropriately.
             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -31,11 +36,11 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
+        
         container = NSPersistentContainer(name: "SwiftCalendar")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        if inMemory { container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null") }
+        
+        container.loadPersistentStores { storeDescription, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -50,7 +55,8 @@ struct PersistenceController {
                  */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        })
+        }
+        
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
