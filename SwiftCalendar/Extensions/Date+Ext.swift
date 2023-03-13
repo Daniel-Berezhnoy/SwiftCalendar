427e3a67a6b0614f88e8349bbb10c3c63d1c74cd
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-03-12 22:08:09 -0700

    Added Date+Ext

--- SwiftCalendar/Extensions/Date+Ext.swift
+++ SwiftCalendar/Extensions/Date+Ext.swift
@@ -5,4 +5,47 @@
 //  Created by Daniel Berezhnoy on 3/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+extension Date {
+    var startOfMonth: Date {
+        Calendar.current.dateInterval(of: .month, for: self)!.start
+    }
+
+    var endOfMonth: Date {
+        Calendar.current.dateInterval(of: .month, for: self)!.end
+    }
+
+    var endOfDay: Date {
+        Calendar.current.dateInterval(of: .day, for: self)!.end
+    }
+
+    var startOfPreviousMonth: Date {
+        let dayInPreviousMonth = Calendar.current.date(byAdding: .month, value: -1, to: self)!
+        return dayInPreviousMonth.startOfMonth
+    }
+
+    var startOfNextMonth: Date {
+        let dayInNextMonth = Calendar.current.date(byAdding: .month, value: 1, to: self)!
+        return dayInNextMonth.startOfMonth
+    }
+
+    var numberOfDaysInMonth: Int {
+        // endOfMonth returns the 1st of next month at midnight.
+        // An adjustment of -1 is necessary to get last day of current month
+        let endDateAdjustment = Calendar.current.date(byAdding: .day, value: -1, to: self.endOfMonth)!
+        return Calendar.current.component(.day, from: endDateAdjustment)
+    }
+
+    var dayInt: Int {
+        Calendar.current.component(.day, from: self)
+    }
+
+    var monthInt: Int {
+        Calendar.current.component(.month, from: self)
+    }
+
+    var monthFullName: String {
+        self.formatted(.dateTime.month(.wide))
+    }
+}
